b3b67387d5cb74d59c955063f3baf1213420d818
Josiah Hunsinger <fencepost13302@gmail.com>
2020-10-13 19:13:32 -0400

    Getting output from the model (it's random garbage but still :D )

---
+++ infer.py
@@ -0,0 +1,11 @@
+import numpy as np
+import model
+import cv2
+import sys
+
+im = cv2.imread(sys.argv[1])
+input_data = im[np.newaxis, :,:,:]
+input_data = input_data.astype('float')
+
+my_model = model.CaptchaBreaker()
+print( my_model(input_data) )
--- model.py
+++ model.py
@@ -1,12 +1,38 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import activations, layers
 
-cnn_base = models.Sequential()
-cnn_base.add( layers.Conv2D(32, (3,3), activation='relu' )
-cnn_base.add( layers.Conv2D(64, (3,3), activation='relu' )
-ccn_base.add( layers.MaxPool2D((2,2)) )
-cnn_base.add( layers.Conv2D(64, (3,3), activation='relu' )
-cnn_base.add( layers.Conv2D(128, (3,3), activation='relu' )
-ccn_base.add( layers.MaxPool2D((2,2)) )
-cnn_base.add( layers.Conv2D(128, (3,3), activation='relu' )
-cnn_base.add( layers.Conv2D(67, (3,3), activation='relu' ) # 26 letters*2 + 10 digits + 1 objectness + 4 bounding box corrections
+class CaptchaBreaker(tf.keras.Model):
+    def __init__(self):
+        super(CaptchaBreaker, self).__init__()
+        self.backbone_layers = [] 
+        self.backbone_layers.append( layers.Conv2D(32, (3,3), activation='relu') )
+        self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.Conv2D(64, (3,3), activation='relu') )
+        self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.MaxPool2D((2,2)) )
+        self.backbone_layers.append( layers.Conv2D(64, (3,3), activation='relu') )
+        self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.Conv2D(128, (3,3), activation='relu') )
+        self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.MaxPool2D((2,2)) )
+        self.backbone_layers.append( layers.Conv2D(128, (3,3), activation='relu') )
+        self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.Conv2D(67, (3,3) ) )
+
+    def call(self, inputs, training=False):
+        x = inputs
+        for layer in self.backbone_layers:
+            x = layer(x)
+        
+        class_slice      = x[:,:,:,  :62] # 26*2 letters + 10 digits
+        objectness_slice = x[:,:,:,62:63] # objectness
+        box_offset_slice = x[:,:,:,63:65] # (x, y) detection box offset factors from center of cell 
+        box_scale_slice  = x[:,:,:,65:67] # (w, h) detection box scale factors
+
+        class_logits      = activations.sigmoid( class_slice )
+        objectness_logits = activations.sigmoid( objectness_slice )
+        box_offset_factor = activations.sigmoid( box_offset_slice )
+        box_scale_factors = activations.exponential( box_scale_slice )
+
+        return layers.Concatenate(axis=-1)( [class_logits, objectness_logits, box_offset_factor, box_scale_factors] )
+
