56804f835055782c76cb1811a456310a72d981b1
Josiah Hunsinger <fencepost13302@gmail.com>
2020-10-22 19:33:10 -0400

    Data pipline in good state. Model in intentionally crippled state for rapid iteration through training process

--- train.py
+++ train.py
@@ -8,13 +8,15 @@ import cv2
 import sys
 import os
 
-def load_dataset(data_dir, random_seed=42):
-    # Get file locations
-    data_dir = sys.argv[1]
-    sample_paths = [ (os.path.join(data_dir, sample_path), os.path.join(data_dir, sample_path[:-4]+'.json'), sample_path[:-4]) for sample_path in os.listdir(data_dir) if sample_path[-4:] == '.png' ]
-    
-    random.seed( random_seed )
-    random.shuffle( sample_paths )
+
+def load_dataset(sample_paths):
+    # TF likes to convert string it gets its hands on to bytes. Watch out for that. It will break stuff
+    for s, sample in enumerate(sample_paths):
+        sample_as_list = list(sample)
+        for i in range(len(sample_as_list)):
+            if type(sample_as_list[i]) == bytes:
+                sample_as_list[i] = sample_as_list[i].decode()
+        sample_paths[s] = sample_as_list
 
     # Load the first sample just to get dimensions
     im_path, _, _ = sample_paths[0] 
@@ -31,6 +33,7 @@ def load_dataset(data_dir, random_seed=42):
         h = int( math.floor(h / pooling_factor) )
     
     # Load the data
+    random.shuffle(sample_paths)
     for n, (image_path, label_path, letters) in enumerate(sample_paths):
         X = CaptchaBreaker.load_and_preprocess_image(image_path)
         Y = np.zeros( (h, w, 67 + 3), dtype='float32' )
@@ -63,20 +66,51 @@ def load_dataset(data_dir, random_seed=42):
                         Y[i, j, 62] = 0 # This was already the value there but lets make it explicit...
                         Y[i, j, 68] = 1
 
-        yield (X, Y)
+        yield X, Y
 
+def get_dataset_dims_from_generator(gen, args=[]):
+    g = gen(*args)
+    return tuple( map(lambda x:x.shape, next(g)) )
+
+def make_train_and_test_datasets(data_dir, validation_split=0.8):
+    # Get file locations
+    if type(data_dir) == bytes:
+        data_dir = data_dir.decode()
+    sample_paths = [ (os.path.join(data_dir, sample_path), os.path.join(data_dir, sample_path[:-4]+'.json'), sample_path[:-4]) for sample_path in os.listdir(data_dir) if sample_path[-4:] == '.png' ]
+    random.shuffle( sample_paths )
+
+    N_train = int(len(sample_paths) * validation_split)
+    train_sample_paths = sample_paths[:N_train]    
+    train_dataset = tf.data.Dataset.from_generator(
+        load_dataset, 
+        args=[train_sample_paths],
+        output_types=(tf.float32, tf.float32),
+        output_shapes=get_dataset_dims_from_generator(load_dataset, [train_sample_paths]))
+
+    test_sample_paths = sample_paths[N_train:]
+    test_dataset = tf.data.Dataset.from_generator(
+        load_dataset, 
+        args=[test_sample_paths],
+        output_types=(tf.float32, tf.float32),
+        output_shapes=get_dataset_dims_from_generator(load_dataset, [test_sample_paths]))
+    
+    return train_dataset, test_dataset
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python ./train.py <data_dir>')
 
     # Load data
-    dataset = tf.data.Dataset.from_generator(load_dataset, output_types=tf.float32)
+    data_dir = sys.argv[1]
+    train_dataset, test_dataset = make_train_and_test_datasets( data_dir )
 
     # Compile and fit model
     model = CaptchaBreaker()
     model.compile(optimizer='Adam', loss=CaptchaBreaker.yolo_loss)
-    model.fit(X_train, Y_train, batch_size=100, epochs=5, validation_data=(X_test, Y_test), validation_batch_size=100)
+    model.fit(
+        train_dataset.batch(100), 
+        epochs=2, 
+        validation_data=test_dataset.batch(100) )
 
     # Save :D
     model.save('saves/model_v3')
