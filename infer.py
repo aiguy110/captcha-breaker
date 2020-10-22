Fixed issue where was normalizing inputs for training but not inference. Still getting garbage but at least it looks like best-attempt garbage instead of total nonsense

infer.py
@@ -6,29 +6,47 @@ import json
 import cv2
 import sys
 
-im = cv2.imread(sys.argv[1])
+im = model.CaptchaBreaker.load_and_preprocess_image(sys.argv[1])
 input_data = im[np.newaxis, :,:,:]
-input_data = input_data.astype('float32')
 
-my_model = models.load_model('saves/model_v2', custom_objects={'yolo_loss': model.CaptchaBreaker.yolo_loss})
+my_model = models.load_model('saves/model_v3', custom_objects={'yolo_loss': model.CaptchaBreaker.yolo_loss})
 model_output = my_model.predict(input_data)
-print(model_output.shape)
 
-best_cell = np.zeros(67, dtype='float32')
-best_offset = np.zeros(2, dtype='int')
+objectness_thres = 0.2
+detections = []
 for i in range(model_output.shape[1]):
     for j in range(model_output.shape[2]):
-        if model_output[0, i, j, 62] > best_cell[62]:
-            best_cell = model_output[0, i, j, :]
-            best_offset = np.array([i, j], dtype='int')
+        if model_output[0, i, j, 62] > objectness_thres:
+            detection = model.CaptchaBreaker.decode_rect(model_output[0, i, j, 63:], np.array([j, i], dtype='int'), 8)
+            highest_val = 0
+            highest_ind = 0
+            for l in range(62):
+                if model_output[0, i, j, l] > highest_val:
+                    highest_val = model_output[0, i, j, l]
+                    highest_ind = l
+            detection['letter'] = model.CaptchaBreaker.alphanum[highest_ind]
+            detection['objectness'] = float(model_output[0, i, j, 62])
+            detections.append( detection )
 
 with open('test.json', 'w') as f:
-    highest_val = 0
-    highest_ind = 0
-    for i in range(62):
-        if best_cell[i] > highest_val:
-            highest_val = best_cell[i]
-            highest_ind = i
-    print(best_offset)
-    print(model.CaptchaBreaker.alphanum[i])
-    json.dump( model.CaptchaBreaker.decode_rect(best_cell[63:], best_offset, 8), f )
+    json.dump(detections, f)
+
+# best_cell = np.zeros(67, dtype='float32')
+# best_offset = np.zeros(2, dtype='int')
+# for i in range(model_output.shape[1]):
+#     for j in range(model_output.shape[2]):
+#         if model_output[0, i, j, 62] > best_cell[62]:
+#             best_cell = model_output[0, i, j, :]
+#             best_offset = np.array([j, i], dtype='int')
+
+# with open('test.json', 'w') as f:
+#     highest_val = 0
+#     highest_ind = 0
+#     for i in range(62):
+#         if best_cell[i] > highest_val:
+#             highest_val = best_cell[i]
+#             highest_ind = i
+#     print(best_offset)
+#     print(best_cell[63:])
+#     print(model.CaptchaBreaker.alphanum[highest_ind])
+#     json.dump( [model.CaptchaBreaker.decode_rect(best_cell[63:], best_offset, 8)], f )

model.py
@@ -1,3 +1,4 @@
+import cv2
 import string
 import numpy as np
 import tensorflow as tf
@@ -11,23 +12,19 @@ class CaptchaBreaker(tf.keras.Model):
         super(CaptchaBreaker, self).__init__()
         self.backbone_layers = [] 
         self.backbone_layers.append( layers.Conv2D(64, (3,3), activation='relu', padding='same') )
-        #self.backbone_layers.append( layers.BatchNormalization() )
         self.backbone_layers.append( layers.Conv2D(128, (3,3), activation='relu', padding='same') )
-        #self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.Conv2D(128, (3,3), activation='relu', padding='same') )
         self.backbone_layers.append( layers.MaxPool2D((2,2)) )
         self.backbone_layers.append( layers.Conv2D(128, (3,3), activation='relu', padding='same') )
-        #self.backbone_layers.append( layers.BatchNormalization() )
         self.backbone_layers.append( layers.Conv2D(256, (3,3), activation='relu', padding='same') )
-        #self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.Conv2D(256, (3,3), activation='relu', padding='same') )
         self.backbone_layers.append( layers.MaxPool2D((2,2)) )
         self.backbone_layers.append( layers.Conv2D(256, (3,3), activation='relu', padding='same') )
-        #self.backbone_layers.append( layers.BatchNormalization() )
-        self.backbone_layers.append( layers.Conv2D(256, (3,3), padding='same') ) 
-        #self.backbone_layers.append( layers.BatchNormalization() )
+        self.backbone_layers.append( layers.Conv2D(512, (3,3), padding='same') ) 
+        self.backbone_layers.append( layers.Conv2D(512, (3,3), padding='same') ) 
         self.backbone_layers.append( layers.MaxPool2D((2,2)) )
-        self.backbone_layers.append( layers.Conv2D(256, (3,3), activation='relu', padding='same') )
-        #self.backbone_layers.append( layers.BatchNormalization() )
-        self.backbone_layers.append( layers.Conv2D(67, (3,3), padding='same') ) 
+        self.backbone_layers.append( layers.Conv2D(512, (3,3), activation='relu', padding='same') )
+        self.backbone_layers.append( layers.Conv2D(67, (3,3), padding='same') )
 
     def call(self, inputs, training=False):
         x = inputs
@@ -42,8 +39,8 @@ class CaptchaBreaker(tf.keras.Model):
 
             class_activations      = activations.softmax( class_slice )
             objectness_activations = activations.sigmoid( objectness_slice )
-            box_offset_factors     = activations.sigmoid( box_offset_slice )
-            box_scale_factors      = activations.exponential( box_scale_slice )
+            box_offset_factors     = box_offset_slice#activations.sigmoid( box_offset_slice )
+            box_scale_factors      = box_scale_slice#activations.exponential( box_scale_slice )
 
             return layers.Concatenate(axis=-1)( [class_activations, objectness_activations, box_offset_factors, box_scale_factors] )
         else:
@@ -73,12 +70,12 @@ class CaptchaBreaker(tf.keras.Model):
         
         rect_width  = rect['right' ] - rect['left']
         rect_height = rect['bottom'] - rect['top' ]
-        scale_activations = np.log( np.array([rect_width, rect_height]) / scale_prior )
+        scale_pre_activations = np.log( np.array([rect_width, rect_height]) / scale_prior )
 
         normalized_offset = ( CaptchaBreaker.rect_center( rect ) - cell_offset * cell_size ) / cell_size
-        offset_activations = np.log( (1-normalized_offset) / normalized_offset )
+        offset_pre_activations = np.log( (1-normalized_offset) / normalized_offset )
 
-        return np.concatenate( (offset_activations, scale_activations) )
+        return np.concatenate( (offset_pre_activations, scale_pre_activations) )
 
     @staticmethod
     def rect_center(rect):
@@ -111,3 +108,12 @@ class CaptchaBreaker(tf.keras.Model):
         bounding_box_loss_final = tf.math.reduce_sum( tf.math.multiply(bounding_box_sum_square_diffs, Y_true[:, :, :, 69]), axis=[1,2] )
 
         return classifier_loss_final + objectness_loss_final + bounding_box_loss_final
+    
+    @staticmethod
+    def load_and_preprocess_image(image_path):
+        im = cv2.imread(image_path)
+        X = np.zeros(im.shape, dtype='float32')
+        for c in range(3):
+            X[:,:,c] = im[:,:,c] / np.amax(im[:,:,c])
+        
+        return X 

train.py
@@ -8,14 +8,6 @@ import cv2
 import sys
 import os
 
-def load_and_preprocess_image(image_path):
-    im = cv2.imread(image_path)
-    X = np.zeros(im.shape, dtype='float32')
-    for c in range(3):
-        X[:,:,c] = im[:,:,c] / np.amax(im[:,:,c])
-    
-    return X 
-
 def load_dataset(data_dir, validation_split=0.8):
     # Get file locations
     data_dir = sys.argv[1]
@@ -46,7 +38,7 @@ def load_dataset(data_dir, validation_split=0.8):
 
     # Load the data
     for n, (image_path, label_path, letters) in enumerate(sample_paths):
-        X = load_and_preprocess_image(image_path)
+        X = CaptchaBreaker.load_and_preprocess_image(image_path)
         Y = np.zeros( (h, w, 67 + 3), dtype='float32' )
 
         with open(label_path) as f: 
@@ -102,7 +94,7 @@ if __name__ == '__main__':
     # Compile and fit model
     model = CaptchaBreaker()
     model.compile(optimizer='Adam', loss=CaptchaBreaker.yolo_loss)
-    model.fit(X_train, Y_train, batch_size=100, epochs=10, validation_data=(X_test, Y_test), validation_batch_size=100)
+    model.fit(X_train, Y_train, batch_size=100, epochs=5, validation_data=(X_test, Y_test), validation_batch_size=100)
 
     # Save :D
-    model.save('saves/model_v2')
+    model.save('saves/model_v3')